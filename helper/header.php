614cd00e61bbf0e716f28a0d01c38518fb8211ad
Butters <k.butterfield08@gmail.com>
2020-11-25 01:51:36 -0600

    Revamp
    The original was...terrible.  Starting over, but I have new idea for website.

---
+++ helper/header.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The Butterfield</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/nav.css">
+</head>
+<body>
+    <header class="basic">
+
+    </header>
+    <nav>
+        <ul class="navbar basic">
+            <li class="navItem"><a href="index.php">Home</a></li>
+            <li class="navItem"><a href="#projects">Projects</a></li>
+            <li class="navItem"><a href="#aboutMe">About Me</a></li>
+            <li class="navItem"><a href="#contact">Contact Me</a></li>
+            <li class="navItem account"><a href="#login/sign up">Profile</a></li>
+        </ul>
+    </nav>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+    include "helper/header.php";
+?>
+
+<main class="basic">
+
+
+</main>
+
+<?php 
+    include "helper/footer.php";
+?>
+
+
