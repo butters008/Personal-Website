dadef2b21524e35265d7f98255cb0096950d992d
Butters <k.butterfield08@gmail.com>
2021-09-30 10:09:00 -0500

    Update project_info.php
    Moving from PC to laptop

--- project_info.php
+++ project_info.php
@@ -43,46 +43,52 @@
 
 <main class="basic">
 <br>
-    <h2 style="text-align: center;">Current Project</h2>
-    <div class="currentProjectOutline">
-      <!-- Hard coded test values, will be loading this in dynamically -->
-      <img src="Images/webDev.png" alt="Web Development"><!-- TODO: Get images to load dynamically -->
-      <h3><strong>Project Name:</strong> <?php echo $project['project_name']; ?></h3>
-      <!-- This was taken from tutorial previously mentioned -->
-      <!-- Hard coding this in, will probably have to re-work this in the future -->
-      <br>
-      <label>Percentage of Completion: <?php echo $project["project_percentage"]."%";?></label> 
-      <br>
-      <div class="myProgress">
-        <div class="myBar" style="width: <?php echo $project["project_percentage"]; ?>%;"></div>
-      </div>
-      <br>
-      <br>
-      <table>
-        <tr>
-          <td>Project Type:</td>
-          <td><strong><?php echo $project["project_type"]?></strong></td>
-        </tr>
-        <tr>
-          <td>Project Start:</td>
-          <td><strong><?php echo $project_moreInfo["project_start"]?></strong></td>
-        </tr>
-      </table>
-      
-      <p><strong>Short Discription:</strong><br><br>
-        <?php echo $project_moreInfo["project_shortInfo"]; ?>
-      </p>
-      <br>
+<?php 
+if(isset($_SESSION["member"])){ ?>
+  <h2 style="text-align: center;">MEMBER!<button>follow</button></h2> <!-- Change to chechbox so they can follow project -->
+<?php } else { ?>
+  <h2 style="text-align: center;">Current Project</h2>
+<?php } ?>
+  <div class="currentProjectOutline">
+    <!-- Hard coded test values, will be loading this in dynamically -->
+    <img src="Images/webDev.png" alt="Web Development"><!-- TODO: Get images to load dynamically -->
+    <h3><strong>Project Name:</strong> <?php echo $project['project_name']; ?></h3>
+    <!-- This was taken from tutorial previously mentioned -->
+    <!-- Hard coding this in, will probably have to re-work this in the future -->
+    <br>
+    <label>Percentage of Completion: <?php echo $project["project_percentage"]."%";?></label> 
+    <br>
+    <div class="myProgress">
+      <div class="myBar" style="width: <?php echo $project["project_percentage"]; ?>%;"></div>
     </div>
-    <br><br>
+    <br>
+    <br>
+    <table>
+      <tr>
+        <td>Project Type:</td>
+        <td><strong><?php echo $project["project_type"]?></strong></td>
+      </tr>
+      <tr>
+        <td>Project Start:</td>
+        <td><strong><?php echo $project_moreInfo["project_start"]?></strong></td>
+      </tr>
+    </table>
+    
+    <p><strong>Short Discription:</strong><br><br>
+      <?php echo $project_moreInfo["project_shortInfo"]; ?>
+    </p>
+    <br>
+  </div>
+  <br><br>
 
-    <!-- Thinking about doing a table in here, to make use to the entire width -->
-    <!-- Or make a UL list that can go vertically instead of horizantially -->
+  <!-- Thinking about doing a table in here, to make use to the entire width -->
+  <!-- Or make a UL list that can go vertically instead of horizantially -->
 
 
 
-    <h2 style="text-align: center; margin-bottom: none;">Recent Project Update</h2>
-    <div class="currentProjectBlog">
+  <h2 style="text-align: center; margin-bottom: none;">Recent Project Update</h2>
+  <div class="currentProjectBlog">
+
 <?php
     if ($project_blog_rows > 0){
         while ($project_blog = mysqli_fetch_assoc($result3)){
@@ -94,8 +100,8 @@
         }
       }
 ?>
-    </div>
-    <br>
+  </div>
+  <br>
 </main>
 
 <?php
