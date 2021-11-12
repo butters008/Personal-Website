7efa1b3b116cf6061c1c1f9117cf659b3af1f2c8
Butters <k.butterfield08@gmail.com>
2021-11-12 13:20:09 -0600

    Moving from PC to Laptop
    Moving from PC to Laptop

--- index.php
+++ index.php
@@ -10,9 +10,61 @@ require_once "includes/functions.inc.php";
   <div id="companyLogo">
     <h2>The Butterfield LLC</h2>
   </div>
-  <div class="timeTracker">
-    
-  </div>
+  <section class="timeTracker">
+    <!-- Taking out div container -->
+    <!-- I want to load this in two ways -->
+    <!-- 1 - load the exact amount of bars needed for projects, ie Project 1 needs 3, project 2 needs 4 and ect -->
+    <!-- 2 - Load values in from DB -->
+
+    <h2>Languages used in Projects</h2>
+    <div class="pieRow">
+      <div class="col-md-3">
+        <div class="skill-name center-block">
+          <div class="chart-container">
+            <div class="chart" data-percent="92" data-bar-color="#23afe3">
+              <span class="percent" data-after="%">92</span>
+            </div>
+          </div>
+          <p>HTML</p>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="skill-name center-block">
+          <div class="chart-container">
+            <div class="chart" data-percent="92" data-bar-color="#23afe3">
+              <span class="percent" data-after="%">92</span>
+            </div>
+          </div>
+          <p>CSS</p>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="skill-name center-block">
+          <div class="chart-container">
+            <div class="chart" data-percent="92" data-bar-color="#23afe3">
+              <span class="percent" data-after="%">92</span>
+            </div>
+          </div>
+          <p>JavaScript</p>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="skill-name center-block">
+          <div class="chart-container">
+            <div class="chart" data-percent="92" data-bar-color="#23afe3">
+              <span class="percent" data-after="%">92</span>
+            </div>
+          </div>
+          <p>PHP</p>
+        </div>
+      </div>
+
+
+    </div>    
+  </sectrion>
   <div class="project">
 
   </div>
