770abc18356a4d73c01c49cc7b860fef37f769a2
Butters <k.butterfield08@gmail.com>
2021-11-23 06:06:57 -0600

    Got basic piechart working
    Got a basic version of the piechart working with hard coded values.  For right now it is ok, but would like to make it take values in dynamically

--- index.php
+++ index.php
@@ -3,76 +3,50 @@
 require_once "helper/header.php";
 require_once "includes/dbh.inc.php";
 require_once "includes/functions.inc.php";
+$sql = "SELECT * FROM projects WHERE project_featured = 1;";
+$result = mysqli_query($conn, $sql);
+$project = mysqli_fetch_assoc($result);
+mysqli_free_result($result);
 ?>
 
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Program', 'Languages used in:'],
+      ['HTML',     5],
+      ['CSS',      3],
+      ['JavaScript',  1],
+      ['PHP', 2],
+      ['MySQL',    1]
+    ]);
 
-<section>
-  <div id="companyLogo">
-    <h2>The Butterfield LLC</h2>
-  </div>
-  <section class="timeTracker">
-    <!-- Taking out div container -->
-    <!-- I want to load this in two ways -->
-    <!-- 1 - load the exact amount of bars needed for projects, ie Project 1 needs 3, project 2 needs 4 and ect -->
-    <!-- 2 - Load values in from DB -->
+    var options = {
+      title: 'Hours of Programming languages used',
+      pieHole: 0.4,
+    };
 
-    <h2>Languages used in Projects</h2>
-    <div class="pieRow">
-      <div class="col-md-3">
-        <div class="skill-name center-block">
-          <div class="chart-container">
-            <div class="chart" data-percent="92" data-bar-color="#23afe3">
-              <span class="percent" data-after="%">92</span>
-            </div>
-          </div>
-          <p>HTML</p>
-        </div>
-      </div>
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+  }
+</script>
 
-      <div class="col-md-3">
-        <div class="skill-name center-block">
-          <div class="chart-container">
-            <div class="chart" data-percent="92" data-bar-color="#23afe3">
-              <span class="percent" data-after="%">92</span>
-            </div>
-          </div>
-          <p>CSS</p>
-        </div>
-      </div>
+<div id="companyLogo">
+  <h2>The Butterfield LLC</h2>
+</div>
+<section id="project">  
 
-      <div class="col-md-3">
-        <div class="skill-name center-block">
-          <div class="chart-container">
-            <div class="chart" data-percent="92" data-bar-color="#23afe3">
-              <span class="percent" data-after="%">92</span>
-            </div>
-          </div>
-          <p>JavaScript</p>
-        </div>
-      </div>
+  <div class="pieTracker">
+  <h2><strong>CURRENT PROJECT: <?php echo $project['project_name']; ?></strong></h2>  
+    <div id="donutchart"></div>
+  </div><!-- End of projectTracker -->
+</section><!-- END OF project  -->
 
-      <div class="col-md-3">
-        <div class="skill-name center-block">
-          <div class="chart-container">
-            <div class="chart" data-percent="92" data-bar-color="#23afe3">
-              <span class="percent" data-after="%">92</span>
-            </div>
-          </div>
-          <p>PHP</p>
-        </div>
-      </div>
+<div class="blog">
 
-
-    </div>    
-  </sectrion>
-  <div class="project">
-
-  </div>
-
-  <div class="blog">
-
-  </div>
-</section>
+</div>
 </main>
 
 <?php
