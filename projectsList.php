89ee7559988523b8b3bb2593b8d9671ee00a4d66
Butters <k.butterfield08@gmail.com>
2021-12-24 13:51:41 -0600

    Picture Working`
    Got the picture working and now it displays the pictures nicely.  Still a little more needs to be done, but mostly done  and rock'n

--- projectsList.php
+++ projectsList.php
@@ -16,7 +16,7 @@ $resultCheck = mysqli_num_rows($result);
     <table class="projectList">
       <thead>
         <tr>
-          <th colspan="2">images</th>
+          <th colspan="2">Project Icon:</th>
           <th colspan="2">Project Status:</th>
           <th colspan="2">Project Name:</th>
           <th colspan="2">Project Type:</th>
@@ -28,7 +28,7 @@ $resultCheck = mysqli_num_rows($result);
       if ($resultCheck > 0){
         while ($project = mysqli_fetch_assoc($result)){
           echo '<tr class="projectRow">';
-          echo "<td colspan='2' class='projectDataPic'><img class='blogPicture' src='Images/'".$project['project_type_pic'].'></td>';
+          echo '<td colspan="2" class="projectDataPic"><img class="blogPicture" src="Images/'.$project['project_type_pic'].'"></td>';
           echo '<td colspan="2" class="projectData"><a href="project_info.php?project_id='.$project["project_id"].'">'.$project["project_status"]."</td>";
           echo '<td colspan="2" class="projectData"><a href="project_info.php?project_id='.$project["project_id"].'">'.$project["project_name"].'</td>';
           echo "<td>".$project["project_type"]."</td>";
